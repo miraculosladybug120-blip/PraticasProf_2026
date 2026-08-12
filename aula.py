711a938c6a7d095c6defb11539157a4bea82dd87
Arthur <cc26549@g.unicamp.br>
2026-08-11 22:36:54 -0300

    aula1

--- aula.py
+++ aula.py
@@ -1,12 +1,11 @@
-import aula2
-
 nome = input("Digite o seu nome>")
 print(f'Voce digitou o nome {nome}')
+
 print()
 print("Vamos desenha")
 
 for x in range(10):
-  print("-", end="")
+      print("-", end="")
   main()
 
   print(f'fim do programa {__main__}')
